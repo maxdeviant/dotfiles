bd05f9308acf18d5f759798b781d4dbe6a1a2e59
Marshall Bowers <elliott.codes@gmail.com>
2016-08-22 22:20:42 -0400

    Add git-log utils

--- osx/config/fish/aliases.fish
+++ osx/config/fish/aliases.fish
@@ -2,6 +2,14 @@ function ll
     ls -al
 end
 
+function gll
+    git log --graph --date=short --pretty=format:'%Cgreen%h %Cblue%cd (%cr) %Cred%an%C(yellow)%d%Creset: %s'
+end
+
+function glll
+    git log --graph --stat --date=short --pretty=format:'%Cgreen%h %Cblue%cd (%cr) %Cred%an%C(yellow)%d%Creset: %s'
+end
+
 function goops
     git add -A
     git reset --hard HEAD
