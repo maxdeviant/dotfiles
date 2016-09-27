cb4a7135b91aace4a47c0ae47b809ac51a8fb862
Marshall Bowers <elliott.codes@gmail.com>
2016-09-26 22:41:26 -0400

    Add conversion shortcuts for Reason

--- osx/config/fish/aliases.fish
+++ osx/config/fish/aliases.fish
@@ -14,3 +14,9 @@ function goops
     git add -A
     git reset --hard HEAD
 end
+
+# Converts ML code into Reason
+alias mlre "pbpaste | refmt -use-stdin true -parse ml -print re -is-interface-pp false | pbcopy"
+
+# Converts Reason code into ML
+alias reml "pbpaste | refmt -use-stdin true -parse re -print ml -is-interface-pp false | pbcopy"
