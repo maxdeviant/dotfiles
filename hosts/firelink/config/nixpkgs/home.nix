d4af05c2599dd1c957ce34c9de7510a7dd4ec16c
Marshall Bowers <elliott.codes@gmail.com>
2019-11-24 20:12:39 -0500

    Configure neovim

--- hosts/firelink/config/nixpkgs/home.nix
+++ hosts/firelink/config/nixpkgs/home.nix
@@ -8,4 +8,51 @@
     userName = "Marshall Bowers";
     userEmail = "elliott.codes@gmail.com";
   };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    extraConfig = ''
+      "
+      " General
+      "
+
+      set encoding=utf-8
+      set fileencoding=utf-8
+
+      set number
+      set ruler
+      set showcmd
+      set autoindent
+
+      filetype plugin indent on
+      set tabstop=4
+      set shiftwidth=4
+      set expandtab
+
+      syntax on
+
+      set ttyfast
+
+      "
+      " Keybinds
+      "
+
+      " Better Escape binding
+      inoremap jj <Esc>
+
+      " Don't use arrow keys!
+      noremap <Up> <NOP>
+      noremap <Down> <NOP>
+      noremap <Left> <NOP>
+      noremap <Right> <NOP>
+
+      " Really, just don't.
+      inoremap <Up> <NOP>
+      inoremap <Down> <NOP>
+      inoremap <Left> <NOP>
+      inoremap <Right> <NOP>
+    '';
+  };
 }
