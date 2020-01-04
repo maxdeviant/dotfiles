b2159e6289a13d7a7baff6ec84bb3bc758dcf154
Marshall Bowers <elliott.codes@gmail.com>
2020-01-04 13:02:55 -0500

    Configure default emoji font

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -48,6 +48,14 @@
     fira-code
   ];
 
+  fonts.fontconfig = {
+    enable = true;
+
+    defaultFonts = {
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
