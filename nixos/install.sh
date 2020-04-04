f25485198a76bb7fb556a71194f3643dcdd0db71
Marshall Bowers <elliott.codes@gmail.com>
2020-04-04 13:13:12 -0400

    Adjust partition names for NVMe vs SATA devices

--- nixos/install.sh
+++ nixos/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 NIXOS_CONFIGURATION_URL=https://raw.githubusercontent.com/maxdeviant/dotfiles/master/nixos/configuration.nix
 
@@ -10,6 +10,12 @@ fi
 if [ -b "$1" ]; then
     BLOCK_DEV=$1
 
+    if [[ "$BLOCK_DEV" == *"nvme"* ]]; then
+        IS_NVME=1
+    else
+        IS_NVME=0
+    fi
+
     echo "Preparing to install NixOS on $BLOCK_DEV..."
 
     #
@@ -18,17 +24,23 @@ if [ -b "$1" ]; then
 
     echo "Partitioning drive..."
 
+    if $IS_NVME; then
+        PARTITION_PREFIX=p
+    else
+        PARTITION_PREFIX=''
+    fi
+
     parted "$BLOCK_DEV" -- mklabel gpt
 
     parted "$BLOCK_DEV" -- mkpart primary 512MiB -8GiB
-    NIXOS_PARTITION="${BLOCK_DEV}p1"
+    NIXOS_PARTITION="${BLOCK_DEV}${PARTITION_PREFIX}1"
 
     parted "$BLOCK_DEV" -- mkpart primary linux-swap -8GiB 100%
-    SWAP_PARTITION="${BLOCK_DEV}p2"
+    SWAP_PARTITION="${BLOCK_DEV}${PARTITION_PREFIX}2"
 
     parted "$BLOCK_DEV" -- mkpart ESP fat32 1MiB 512MiB
     parted "$BLOCK_DEV" -- set 3 boot on
-    BOOT_PARTITION="${BLOCK_DEV}p3"
+    BOOT_PARTITION="${BLOCK_DEV}${PARTITION_PREFIX}3"
 
     #
     # 2.2.3. Formatting
