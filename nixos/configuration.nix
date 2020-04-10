c27e23b03874aa98f990f77a6ef7a8be05490b2d
Marshall Bowers <elliott.codes@gmail.com>
2020-04-10 14:34:03 -0400

    Add Tailscale

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -84,6 +84,7 @@
     psmisc
     rxvt_unicode
     scrot
+    tailscale
     xclip
     wget
 
@@ -114,6 +115,8 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+
+  services.tailscale.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
