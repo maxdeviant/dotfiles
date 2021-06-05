080d320f8b4b484e2e6917515d9e258feaa007eb
Marshall Bowers <elliott.codes@gmail.com>
2021-06-04 22:33:00 -0400

    firelink: Set `pull.rebase` option for Git

--- hosts/firelink/config/nixpkgs/home.nix
+++ hosts/firelink/config/nixpkgs/home.nix
@@ -133,6 +133,9 @@ in
     enable = true;
     userName = "Marshall Bowers";
     userEmail = "elliott.codes@gmail.com";
+    extraConfig = {
+      pull.rebase = false;
+    };
   };
 
   home.packages = with pkgs; [
