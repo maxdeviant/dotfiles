ee3eca909125210b7bb696c0c075405bbba10f7f
Marshall Bowers <elliott.codes@gmail.com>
2021-08-26 22:14:43 -0400

    firelink: Update `easy-purescript-nix`

--- hosts/firelink/devenv/purescript.nix
+++ hosts/firelink/devenv/purescript.nix
@@ -4,8 +4,8 @@ let
   easy-ps = import (fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "7b1c1635e16c7f12065db2f8ec049030fcc63655";
-    sha256 = "1nybcann9aiwbvj95p6wam8xyhxwaxmfnkxmgylxcw42np2lvbzr";
+    rev = "bbef4245cd6810ea84e97a47c801947bfec9fadc";
+    sha256 = "00764zbwhbn61jwb5px2syzi2f9djyl8fmbd2p8wma985af54iwx";
   }) {
     inherit pkgs;
   };
