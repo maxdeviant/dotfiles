c7021e276d1646a1a946b180697832e8438233d7
Marshall Bowers <elliott.codes@gmail.com>
2022-03-12 08:51:22 -0500

    Fix multiple bootloaders warning

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,8 +12,6 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
     enable = true;
